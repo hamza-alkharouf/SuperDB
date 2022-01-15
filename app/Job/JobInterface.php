0ab8a95c046113dd64f1861c7162cbe32c61256d
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-01-15 21:49:21 +0200

    .

--- app/Job/JobInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Job;
-
-interface JobInterface{
-    public function send($isSucceeded,$mysqlConnection);
-}
-
-?>
--- app/RestoreDB/ImportDB/ImportInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\RestoreDB\ImportDB;
- 
-interface ImportInterface {
- public function createTable($tablename,$file,$id);   
-}
-?>
